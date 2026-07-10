2471a3acbe7ea56ecab111c44c0ff99024e73bc9
zkzk5214 <59314355+zkzk5214@users.noreply.github.com>
2026-07-10 09:45:57 +0800

    sp_ratio

--- inferencer/dark_bg_detector.py
+++ inferencer/dark_bg_detector.py
@@ -19,8 +19,10 @@ _dark_bg_cfg = _config.get('dark_bg', {})
 _model_threshold = float(_dark_bg_cfg.get('model_threshold', 0.95))
 _dark_pixel_thresh = int(_dark_bg_cfg.get('dark_pixel_thresh', 50))
 _dark_ratio_thresh = float(_dark_bg_cfg.get('dark_ratio_thresh', 0.75))
-_bright_pixel_thresh = int(_dark_bg_cfg.get('bright_pixel_thresh', 150))
-_bright_ratio_thresh = float(_dark_bg_cfg.get('bright_ratio_thresh', 0.1))
+_bright_thresh = int(_dark_bg_cfg.get('bright_thresh', 80))
+_min_patch_area = int(_dark_bg_cfg.get('min_patch_area', 200))
+_patch_ratio_thresh = float(_dark_bg_cfg.get('patch_ratio_thresh', 0.03))
+_spot_ratio_thresh = float(_dark_bg_cfg.get('spot_ratio_thresh', 0.10))
 _bbox_expand_ratio = float(_dark_bg_cfg.get('bbox_expand_ratio', 0.0))
 _bbox_expand_up_ratio = float(_dark_bg_cfg.get('bbox_expand_up_ratio', 0.1))
 
@@ -37,6 +39,30 @@ def _init():
         _face_detector = Detector(
             weights=os.path.join(RESOURCE_PATH, 'arcface_weights_best_new.onnx'),
             sp=os.path.join(RESOURCE_PATH, 'shape_predictor_68_face_landmarks.dat'))
+
+
+def classify_bright_regions(gray, mask, bright_thresh, min_patch_area):
+    bg_only = gray.copy()
+    bg_only[mask != 255] = 0
+    _, binary = cv2.threshold(bg_only, bright_thresh, 255, cv2.THRESH_BINARY)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
+
+    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(binary, connectivity=8)
+
+    spot_area = 0
+    patch_area = 0
+
+    for i in range(1, num_labels):
+        area = stats[i, cv2.CC_STAT_AREA]
+        if area < min_patch_area:
+            spot_area += area
+        else:
+            patch_area += area
+
+    total = max(len(gray[mask == 255]), 1)
+    return spot_area / total, patch_area / total
 
 
 def detect_bg_darkness(img_bgr, face_bbox):
@@ -61,11 +87,14 @@ def detect_bg_darkness(img_bgr, face_bbox):
 
     bg_pixels = gray[mask == 255]
     if len(bg_pixels) == 0:
-        return False, -1.0, 0.0
+        return False, -1.0, 0.0, 0.0, 0.0
 
     dark_ratio = (bg_pixels < _dark_pixel_thresh).mean()
-    bright_ratio = (bg_pixels > _bright_pixel_thresh).mean()
-    is_dark = bool(dark_ratio > _dark_ratio_thresh) and (bright_ratio < _bright_ratio_thresh)
+    spot_ratio, patch_ratio = classify_bright_regions(
+        gray, mask, _bright_thresh, _min_patch_area)
+    is_dark = bool(dark_ratio > _dark_ratio_thresh) \
+        and patch_ratio < _patch_ratio_thresh \
+        and spot_ratio < _spot_ratio_thresh
 
     if is_dark:
         non_dark = bg_pixels[bg_pixels >= _dark_pixel_thresh]
@@ -78,7 +107,7 @@ def detect_bg_darkness(img_bgr, face_bbox):
     else:
         darkness_level = 0.0
 
-    return bool(is_dark), float(dark_ratio), float(bright_ratio), darkness_level
+    return bool(is_dark), float(dark_ratio), float(spot_ratio), float(patch_ratio), darkness_level
 
 
 def dark_bg_check(img_bgr):
@@ -97,17 +126,16 @@ def dark_bg_check(img_bgr):
     if not face_bboxes:
         return False, StatusCode.NOFACE.value, dark_score, 0.0, 0.0, 0.0
 
-    is_dark, dk_ratio, br_ratio, darkness_level = detect_bg_darkness(img_bgr, face_bboxes[0])
+    is_dark, dk_ratio, spot_ratio, patch_ratio, darkness_level = detect_bg_darkness(img_bgr, face_bboxes[0])
 
     if dk_ratio == -1.0:
-        # background pixel count = 0, fall back to stage1 result
         return True, 100, dark_score, 0.0, 0.0, 0.0
 
-    return is_dark, 100, dark_score, dk_ratio, br_ratio, darkness_level
+    return is_dark, 100, dark_score, dk_ratio, spot_ratio, patch_ratio, darkness_level
 
 
 if __name__ == '__main__':
 
     img_bgr = cv2.imread('./unit_test/test_img/black_bg_test.jpg')
-    is_dark, resp_code, dark_score, dk_ratio, br_ratio, darkness_level = dark_bg_check(img_bgr)
-    print(f'is_dark={is_dark}, resp_code={resp_code}, dark_score={dark_score:.4f}, dk_ratio={dk_ratio:.4f}, br_ratio={br_ratio:.4f}, darkness_level={darkness_level:.4f}')
+    is_dark, resp_code, dark_score, dk_ratio, spot_ratio, patch_ratio, darkness_level = dark_bg_check(img_bgr)
+    print(f'is_dark={is_dark}, resp_code={resp_code}, dark_score={dark_score:.4f}, dk_ratio={dk_ratio:.4f}, spot_ratio={spot_ratio:.4f}, patch_ratio={patch_ratio:.4f}, darkness_level={darkness_level:.4f}')
--- server.py
+++ server.py
@@ -177,18 +177,20 @@ def dark_bg_check_endpoint():
         json_data, img = process_common_request(request)
         res = create_response(json_data)
 
-        is_dark_bg, resp_code, dark_score, dk_ratio, br_ratio, darkness_level = dark_bg_check(img)
+        is_dark_bg, resp_code, dark_score, dk_ratio, spot_ratio, patch_ratio, darkness_level = dark_bg_check(img)
         res['resp_code'] = resp_code
         res['is_dark_bg'] = is_dark_bg
         res['dark_score'] = round(dark_score, 4)
         res['dk_ratio'] = round(dk_ratio, 4)
-        res['br_ratio'] = round(br_ratio, 4)
+        res['spot_ratio'] = round(spot_ratio, 4)
+        res['patch_ratio'] = round(patch_ratio, 4)
         res['darkness_level'] = int(darkness_level)
 
         latency_ms = str(int((time.time() - start_time) * 1000))
         log_request_result(json_data, res['resp_code'], '/dark_bg_check',
                            {'is_dark_bg': res['is_dark_bg'], 'dark_score': res['dark_score'],
-                            'dk_ratio': res['dk_ratio'], 'br_ratio': res['br_ratio'],
+                            'dk_ratio': res['dk_ratio'], 'spot_ratio': res['spot_ratio'],
+                            'patch_ratio': res['patch_ratio'],
                             'darkness_level': res['darkness_level'], 'TimeCost': latency_ms})
         return jsonify(res)
 
